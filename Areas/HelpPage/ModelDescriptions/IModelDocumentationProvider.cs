4e144569d49ac43bbcbbaba756c6474c83446203
Agustin j. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2022-06-02 12:24:41 -0300

    Agregar archivos de proyecto.

---
+++ Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace ReactCrudApi.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}

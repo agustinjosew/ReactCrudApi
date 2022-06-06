1fd2d7bc41c980e93952ab81b1b3793a57b0016b
Agustin j. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2022-06-06 09:32:51 -0300

    Service worker created and WebApiCopnfig modified

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace ReactCrudApi
 {
@@ -25,6 +21,9 @@ namespace ReactCrudApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
